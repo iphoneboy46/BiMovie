5b35a2463ee9949a6d9bc102b96d72a99f8802bd
iphoneboy46 <64023175+iphoneboy46@users.noreply.github.com>
2022-08-17 23:05:16 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import 'swiper/css';
+import './assets/boxicons-2.1.2/css/boxicons.min.css';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
+
+import {publicRoutes}  from './config/Routes'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <BrowserRouter>
+         <Header />
+          <Routes>
+            {publicRoutes.map((route, index) =>{
+                const Page = route.component;
+                return (
+                    <Route key={index} path={route.path} element={<Page />}  />
+                )
+            })}
+          </Routes>
+         <Footer />
+      </BrowserRouter>
     </div>
   );
 }
